aac2ad305d97ba933e4a4009185a5c2c82e37e44
Ryan Lee <marble92@sbcglobal.net>
2018-01-16 15:46:59 -0600

    homework functionalitk functionality added and homework completed

--- key.js
+++ key.js
@@ -1,4 +1,6 @@
-console.log('this is loaded');
+// Debugging
+// console.log('this is loaded');
+
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
--- liri.js
+++ liri.js
@@ -1,13 +1,11 @@
 // Access Keys for Spotfy and Twitter
 //================================================================
 require("dotenv").config();
-var key = require('./key')
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var key = require('./key.js')
 
 // Packages and Modules
 //================================================================
-var spotify = require('spotify');
+var Spotify = require('node-spotify-api');
 var twitter = require('twitter');
 var request = require('request');
 
@@ -25,7 +23,7 @@ var movieRating;
 var movieCountry;
 var movieLang;
 var moviePlot;
-var movieActor;
+var movieActors;
 
 var parsedData;
 
@@ -34,26 +32,190 @@ var parsedData;
 
 function twitterData() {
 
+	// User Authentication
+	var client = new twitter(key.twitter);
+
+	// Search Parameters, By Username
+	// Returns a maximum of 5 tweets
+	var param = {
+		screen_name: "harpsichordhk",
+		count: 21
+	};
+
+	// Gets most recent tweets posted by the user
+	//
+	client.get('statuses/user_timeline', param, function(error, tweets, response) {
+
+		//If there is no error
+		if (!error) {
+		
+			//Returns 5 of the user's most recent tweets
+			for (var i = 1; i <= 20; i++) {
+				console.log('Tweet #' + i + ':');
+				console.log(tweets[i].text);
+				console.log('Created on: ' + tweets[i].created_at);
+				console.log('=========================')
+			}
+		}
+
+		// Else, display an error
+		else {
+			console.log(error);
+		}
+	});
+
 }
 
 function spotifyData() {
 
+var spotify = new Spotify(key.spotify);
+
+	// If the user inputs a song
+	if (process.argv.length >=4 || typeof userChoice === 'string') {
+
+		//Display spotify song
+		spotify.search({type:'track', query: userChoice}, function(err, data) {
+
+			//If there is no error
+			if (!err) {
+				displaySpotify(data);
+			}
+
+			else {
+				console.log(err);
+			}
+
+		});
+	}
+
+	// If user doesn't input a song
+	else if (process.argv.length < 4) {
+
+		//Display default song
+		spotify.search({type:'track', query: "The Sign Ace of Base" }, function(err, data){
+
+			//If there is no error
+			if (!err) {
+
+				//Display Song data
+				displaySpotify(data);
+			}
+
+			else {
+				console.log(err);
+			}
+		});
+	}
 }
 
-function displaySpotify() {
+function displaySpotify(data) {
 
+	// Variables
+	var artist = data.tracks.items[1].artists[0].name;
+	var track = data.tracks.items[1].name;
+	var album = data.tracks.items[1].album.name;
+	var songPreview = data.tracks.items[1].external_urls.spotify;
+
+	//Log the information into the terminal
+	console.log('Artist: ' + artist);
+	console.log('Track: ' + track);
+	console.log('Album: ' + album);
+	console.log('Song Preview: ' + songPreview);
 }
 
 function movieData() {
 
+	if (process.argv.length >=4 || typeof userChoice === 'string')
+
+		request('http://www.omdbapi.com/?t=' + userChoice + '&apikey=trilogy', function(error, response, body) {
+
+			// If there is no error
+			if (!error && response.statusCode == 200) {
+				parsedData = JSON.parse(body);
+				displayMovie(parsedData);
+			}
+
+			else {
+				console.log(error);
+			}
+
+		});
+
+	else if (process.argv.length < 4) {
+		request('http://www.omdbapi.com/?t=' + 'Mr. Nobody' + '&apikey=trilogy', function(error, response, body) {
+
+			// If there is no error
+
+			if (!error && response.statusCode == 200) {
+				parsedData = JSON.parse(body);;
+				displayMovie(parsedData);
+			}
+
+			else {
+				console.log(error);
+			}
+
+		});
+	}
 }
 
 function displayMovie() {
+
+	// Parsed Variables
+	var movieTitle = parsedData.Title;
+	var movieYear = parsedData.Year;
+	var movieimdbRating = parsedData.imdbRating;
+	var movieTomatoRating = parsedData.Ratings[1].Value;
+	var movieCountry = parsedData.Country;
+	var movieLang = parsedData.Language;
+	var moviePlot = parsedData.Plot;
+	var movieActors = parsedData.Actors;
+
+	//Display movie info
+	console.log('Movie Title: ' + movieTitle);
+	console.log('Release Date: ' + movieYear);
+	console.log('IMDB Rating: ' + movieimdbRating);
+	console.log('Rotten Tomatoes Rating: ' + movieTomatoRating)
+	console.log('Country: ' + movieCountry);
+	console.log('Language: ' + movieLang);
+	console.log('Plot: ' + moviePlot);
+	console.log('Actors: ' + movieActors);
 
 }
 
 function readData() {
 
+var fs = require('fs');
+
+	fs.readFile('random.txt', 'utf8', function(err, data) {
+
+		// If there is no error
+		if (!err) {
+			var dataSplit = data.split(',');
+			userInput = dataSplit[0];
+			userChoice = dataSplit[1];
+
+			switch(userInput) {
+
+				case "my-tweets":
+					twitterData();
+					break;
+
+				case "spotify-this-song":
+					spotifyData();
+					break;
+
+				case "movie-this":
+					movieData();
+					break;
+			}
+		}
+
+		else {
+			console.log(err);
+		}
+
+	});
 }
 
 // Main Program
